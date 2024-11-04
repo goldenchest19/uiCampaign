ebcb9352899a018770eb58017a55cca69cb324af
goldenchest19 <kirya-ignatev-2016@mail.ru>
2024-11-04 22:09:40 +0300

    init commit

---
+++ src/components/PortfolioChart.jsx
@@ -0,0 +1,5 @@
+export default function PortfolioChart() {
+    return (
+        <div>111</div>
+    )
+}
---
+++ src/data-campaign.js
@@ -0,0 +1,25 @@
+export const campaigns = {
+    result: [
+        {
+            id: 1,
+            name: "name1",
+            device_id: 1,
+            banners: [1,2,3,4],
+            budget: 10000,
+        }, 
+        {
+            id: 2,
+            name: "name1",
+            device_id: 1,
+            banners: [1, 2, 3, 4],
+            budget: 10000,
+        },
+        {
+            id: 2,
+            name: "name1",
+            device_id: 1,
+            banners: [1, 2, 3, 4],
+            budget: 10000,
+        }
+    ]
+}
